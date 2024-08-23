beedafae2d3137eb1603962d11d9ba29e3960242
Michael Cronin <michael.cronin90@gmail.com>
2024-08-22 22:45:43 -0400

    adding scroll animations

--- script.js
+++ script.js
@@ -43,8 +43,29 @@ $(document).ready(function() {
         .to(detailContent, {yPercent: 0}, 0.2);
 
         detailImage.removeEventListener("load", onLoad);
-        document.addEventListener('click', hideDetails);
+        window.addEventListener('click', hideDetails);
     };
+
+    function reveal() {
+        console.log("Scrolled!");
+        var reveals = document.querySelectorAll(".reveal");
+        
+      
+        for (var i = 0; i < reveals.length; i++) {
+          var windowHeight = window.innerHeight;
+          var elementTop = reveals[i].getBoundingClientRect().top;
+          var elementVisible = 150;
+      
+          if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+          } else {
+            reveals[i].classList.remove("active");
+          }
+        }
+    }
+      
+    window.addEventListener("scroll", reveal, {passive: true});
+
 
     // Change image and text
     const data = item.dataset;
